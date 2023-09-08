9fee5cf53252d0a964006a8262c39269c23a78bf
Suresh Babu V <144363322+SureshBabu03@users.noreply.github.com>
2023-09-08 11:05:32 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,45 +1,54 @@
-# Function to perform addition
-def add(x, y):
-    return x + y
+import tkinter as tk
 
-# Function to perform subtraction
-def subtract(x, y):
-    return x - y
+# Function to update the display
+def button_click(number):
+    current = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(0, current + str(number))
 
-# Function to perform multiplication
-def multiply(x, y):
-    return x * y
+# Function to perform calculations
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(0, str(result))
+    except Exception:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Error")
 
-# Function to perform division
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return x / y
+# Create the main window
+window = tk.Tk()
+window.title("Calculator")
 
-# Main calculator loop
-while True:
-    print("Options:")
-    print("Enter 'add' for addition")
-    print("Enter 'subtract' for subtraction")
-    print("Enter 'multiply' for multiplication")
-    print("Enter 'divide' for division")
-    print("Enter 'exit' to end the program")
-    
-    user = input(": ")
-    
-    if user == "exit":
-        break
-    elif user in ("add", "subtract", "multiply", "divide"):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        
-        if user == "add":
-            print("Result:", add(num1, num2))
-        elif user == "subtract":
-            print("Result:", subtract(num1, num2))
-        elif user== "multiply":
-            print("Result:", multiply(num1, num2))
-        elif user == "divide":
-            print("Result:", divide(num1, num2))
+# Create an entry widget to display input and results
+entry = tk.Entry(window, width=20)
+entry.grid(row=0, column=0, columnspan=4)
+
+# Define button labels
+button_labels = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', 'C', '=', '+'
+]
+
+# Create buttons
+row_val = 1
+col_val = 0
+buttons = []
+for label in button_labels:
+    if label == '=':
+        button = tk.Button(window, text=label, width=5, command=calculate)
+    elif label == 'C':
+        button = tk.Button(window, text=label, width=5, command=lambda: entry.delete(0, tk.END))
     else:
-        print("Invalid input")
+        button = tk.Button(window, text=label, width=5, command=lambda label=label: button_click(label))
+    button.grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+    buttons.append(button)
+
+# Start the GUI event loop
+window.mainloop()
